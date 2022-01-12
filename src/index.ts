101e62bdf2bac2ac00a73289f57a0ba3eed4d456
Gustavonvp <gustavonvp@gmail.com>
2022-01-12 00:23:21 -0300

    characters bug's on interface and abstract,,,

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,24 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
-
-const numbersCollection = new NumbersCollection([10,3,-5,0,-2]);
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
+const numbersCollection = new NumbersCollection([3,2,1,-5,20,4,7,12]);
+numbersCollection.sort();
 console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection(' x a e t');
+charactersCollection.sort();
+console.log(charactersCollection.data);
+
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+
+linkedList.sort();
+linkedList.print();
+
